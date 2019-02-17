2e1754191c6e52c9996ab3718cb25956a6ce7f44
Rui Esteves <ruicarest@gmail.com>
2019-02-17 18:53:43 +0000

    still not working on redux

---
+++ build/precache-manifest.5dc0d371834473aff9cd8fe07fb2b30c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "bcefac647a5fca550116",
+    "url": "/ruicarest/redux-faster/static/js/runtime~main.bcefac64.js"
+  },
+  {
+    "revision": "d361c43dac0debcd0b97",
+    "url": "/ruicarest/redux-faster/static/js/main.d361c43d.chunk.js"
+  },
+  {
+    "revision": "35f714892e90036e450c",
+    "url": "/ruicarest/redux-faster/static/js/2.35f71489.chunk.js"
+  },
+  {
+    "revision": "846f1431303cb73369880f3f3381c4ff",
+    "url": "/ruicarest/redux-faster/index.html"
+  }
+];
---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/ruicarest/redux-faster/precache-manifest.5dc0d371834473aff9cd8fe07fb2b30c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/ruicarest/redux-faster/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
